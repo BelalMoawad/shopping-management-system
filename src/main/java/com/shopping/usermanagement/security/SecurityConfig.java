8a5ee1c4eb287b9c568d8a75807eafbaa5de4a48
BelalMoawad <belalmoawad1999@gmail.com>
2025-03-30 15:15:36 +0200

    adding simple authorization

--- src/main/java/com/shopping/usermanagement/security/SecurityConfig.java
+++ src/main/java/com/shopping/usermanagement/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.shopping.usermanagement.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -29,5 +33,23 @@ public class SecurityConfig {
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
+    }
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authz) -> authz
+            	.requestMatchers("/api/v1/user/**").hasRole("ADMIN")
+            	.requestMatchers("/api/v1/role/**").hasRole("ADMIN")
+            	.requestMatchers("/api/v1/category/**").hasRole("ADMIN")
+            	.requestMatchers("/api/v1/product/**").hasRole("ADMIN")
+            	.requestMatchers("/api/v1/image/**").hasRole("ADMIN")
+            	.requestMatchers("/api/v1/cart/**").hasRole("USER")
+            	.requestMatchers("/api/v1/cart_item/**").hasRole("USER")
+            	.requestMatchers("/api/v1/order/**").hasRole("USER")
+                .anyRequest().authenticated()
+            )
+            .httpBasic(withDefaults());
+        return http.build();
     }
 }
